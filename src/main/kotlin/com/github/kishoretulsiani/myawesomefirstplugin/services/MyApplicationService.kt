dff138d6051eed0bbb4ebe26126abecb4a6db063
GitHub Action <action@github.com>
2021-01-18 23:22:15 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kishoretulsiani/myawesomefirstplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kishoretulsiani.myawesomefirstplugin.services
+
+import com.github.kishoretulsiani.myawesomefirstplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/kishoretulsiani/myawesomefirstplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kishoretulsiani/myawesomefirstplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kishoretulsiani.myawesomefirstplugin.services
 
+import com.github.kishoretulsiani.myawesomefirstplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
